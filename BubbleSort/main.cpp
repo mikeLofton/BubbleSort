128ca99e23ed2c4ef88fe9a160042a7a5cb2b425
mikeLofton <michaellofton32@gmail.com>
2022-01-24 14:24:11 -0600

    insertion sort

--- BubbleSort/main.cpp
+++ BubbleSort/main.cpp
@@ -28,11 +28,42 @@ void bubbleSort(int numArray[], int arrSize)
 	printArray(numArray, arrSize);
 }
 
+void insertionSort(int arr[], int arrSize)
+{
+	int j, key;
+
+	//For each value in array
+	for (int i = 1; i < arrSize; i++)
+	{
+		//Set key to value at the current index
+		key = arr[i];
+		//Set j to be the previous index
+		j = i - 1;
+
+		//While j is greater than or equal to 0 and the value at j is greater than the key
+		while (j >= 0 && arr[j] > key)
+		{
+			//Set value at the index of j + 1 to be the value at the index of j
+			arr[j + 1] = arr[j];
+			//Decrement j
+			j--;
+		}
+		//Set value at the index of j + 1 to be the key
+		arr[j + 1] = key;
+	}
+
+	printArray(arr, arrSize);
+}
+
 int main()
 {
-	int numbers[] = { 5, 3, 1, 4, 9, 7 };
+	/*int numbers[] = { 5, 3, 1, 4, 9, 7 };*/
 
-	bubbleSort(numbers, 6);
+	int altNumbers[] = { 45, 12, 3, 20, 66, 18, 7 };
+
+	/*bubbleSort(numbers, 6);*/
+
+	insertionSort(altNumbers, 7);
 
 	return 0;
 }
